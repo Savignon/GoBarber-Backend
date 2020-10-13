84a8b10f698d8533b9b63a352d4347eed61e3e1a
Romulo <savignon@gmail.com>
2020-10-13 14:57:13 -0300

    Upload

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ses' | 'ethereal';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'mailing@savignon.com.br',
+      name: 'Equipe GoBarber',
+    },
+  },
+} as IMailConfig;
